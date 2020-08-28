22543d734a58d547a2c7d797a9dba919955e47f2
j0shm1lls <58683930+j0shm1lls@users.noreply.github.com>
2020-08-28 00:55:33 -0700

    Add files via upload
    Added tracking for Dead Matter Dedicated Server RAM usage.  The DM Server will be automatically restarted if RAM use exceeds 25gb (typically when server hangs occur).

--- dmserver.ps1
+++ dmserver.ps1
@@ -1,8 +1,8 @@
 ﻿#Steam Username
-$username = 'YourSteamUsername'
+$username = 'SteamUserName'
 
 #Steam Password
-$pwd = 'YourSteamPassword'
+$pwd = 'SteamPassword'
 
 #path to SteamCMD.exe install
 $SteamCMD = 'c:\SteamCMD\steamcmd.exe'
@@ -18,11 +18,12 @@ $AppID = '1110990'
 #Checking if Steam Guard is active and prompting for Steam Guard key if it is
 write-host "`n Performing first boot check for SteamGuard...`n" -ForeGroundColor White
 Start-Process -FilePath $SteamCMD -ArgumentList ('+login',$username,$pwd,'+exit') -NoNewWindow -Wait 
-
+cls
 #main loop
 while($true)
 {
 $i++
+#Checking for DM Server Updates
 $SteamCMDLog = "$env:Temp\SteamCMDLog.log"
     if (Test-Path $SteamCMDLog){del $SteamCMDLog}
 	        write-host "`n Checking for Dead Matter Dedicated Server update...`n" -ForeGroundColor White
@@ -43,10 +44,24 @@ $updatedm = Get-Content $SteamCMDLog -Raw
         $updatedm = $null
 }}
 Start-Sleep -s 3
-	write-host "`n `(`•̪`●`)`=`╤`─`─ Dead Matter Dedicated Server launched at: $(Get-Date) `-`n" -ForeGroundColor White
-Start-Process -FilePath "$DMDediPath\deadmatterServer.exe" -ArgumentList "-USEALLAVAILABLECORES -log" -Wait
-	write-host " Server closed at: $(Get-Date)`n" -ForeGroundColor White
+#Launching DM Server
+write-host "`n `(o_O`)`=`╤`─`─ Dead Matter Dedicated Server launched at: $(Get-Date) `-`n" -ForeGroundColor White
+Start-Process -FilePath "$DMDediPath\deadmatterServer.exe" -ArgumentList "-USEALLAVAILABLECORES -log"
+start-sleep -s 2
+$p = "deadmatterserver-win64-test"
+while (Get-Process $P -ErrorAction SilentlyContinue){
+Do
+{
+#Calculating and displaying DM Server Ram Use in GB
+write-host -NoNewline "`r Dead Matter Dedicated Server is currently using:" $([math]::Round($(($DMRamUSE = Get-Process $P -ErrorAction SilentlyContinue | select -ExpandProperty WS)/1Gb),2))"GB of RAM...     "
+$proc = Get-Process $p -ErrorAction SilentlyContinue
+start-sleep -s 2
+} While ((Get-Process $P -ErrorAction SilentlyContinue) -and ($proc.WS/1Gb) -lt 25)
+#Killing DM Server if RAM use exceeds 25GB
+kill -processname $p -ErrorAction SilentlyContinue}
+write-host "`n`n Dead Matter Dedicated Server exceeded 25GB of RAM Use (and was killed) or the server was shut down, restarting...`n" -ForegroundColor Red
+start-sleep -s 2
 Select-String -Path "$DMDediPath\deadmatter\Saved\Logs\deadmatter.log" -Pattern 'LogCore'-AllMatches | Foreach {$_.Line}
-	write-host "`n Server shutdown or crashed, Restarting...`n" -ForegroundColor black -BackgroundColor red
-Start-Sleep -s 3
+write-host "`n`n Server closed at: $(Get-Date)`n" -ForeGroundColor White
+start-sleep -s 5
 }
